b307ad7d090e7291f7e2ca28991dfc1482e5886d
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-02-16 11:28:37 +0700

    enhance-GrantingAccess

---
+++ Com.Ambassador.Service.Auth.Lib/ViewModels/AccoutChangePassViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Ambassador.Service.Auth.Lib.ViewModels
+{
+    public class AccoutChangePassViewModel
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
